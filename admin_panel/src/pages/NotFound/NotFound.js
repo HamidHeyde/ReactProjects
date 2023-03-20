a2e3d25412025a7c2b509ed7bdfa034287f3887c
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2023-03-20 15:23:26 -0700

    added basic routing, Home, Login, 404

---
+++ admin_panel/src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './NotFound.scss';
+
+export const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h1>404</h1>
+            <p>Page not found</p>
+        </div>
+    )
+}
